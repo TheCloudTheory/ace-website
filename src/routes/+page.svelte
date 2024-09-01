49bf6e632558825ff38023e49dc1ebf8fd1e0553
Kamil Mrzygłód <kamil@thecloudtheory.com>
2024-09-01 11:05:34 +0200

    New images and content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
     import hero2 from '$lib/assets/hero-2.PNG';
     import feat1 from '$lib/assets/feat-1.PNG';
     import feat2 from '$lib/assets/feat-2.PNG';
+    import feat3 from '$lib/assets/feat-3.PNG';
+    import feat4 from '$lib/assets/feat-4.PNG';
+    import feat5 from '$lib/assets/feat-5.PNG';
 </script>
 
 <svelte:head>
@@ -10,7 +13,7 @@
 	<meta name="description" content="Azure Cost Estimator is a FOSS project aimed at providing cost estimations for Azure infrastructure." />
     <style>
         .hero img {
-            box-shadow: 0px 0px 10px #fff;
+            box-shadow: 0px 0px 20px #9af2ff;
             position: absolute;
         }
 
@@ -20,8 +23,10 @@
         }
 
         img.img2 {
-            top: -7rem;
-            left: 15rem;
+            top: -16rem;
+            left: 14rem;
+            width: 95%;
+            z-index: 1;
         }
 
         .hero {
@@ -104,7 +109,9 @@
                     Azure Cost Estimator provides you with a human-friendly output. It shows you the cost estimation in a way that is easy to understand. You can see the cost of each resource, the total cost of the infrastructure, and the cost of the infrastructure per month. You can also decide if you want to see the cost in USD or any other currency and select formatting which suits you best.
                 </p>
             </div>
-            <div class="column"></div>
+            <div class="column">
+                <img src={feat3} alt="Azure Cost Estimator" />
+            </div>
         </div>
     </div>
 </section>
@@ -112,7 +119,9 @@
 <section class="section">
     <div class="container">
         <div class="columns is-centered">
-            <div class="column"></div>
+            <div class="column">
+                <img src={feat4} alt="Azure Cost Estimator" />
+            </div>
             <div class="column">
                 <h2 class="title">No external dependencies</h2>
                 <p>
@@ -132,7 +141,9 @@
                     Azure Cost Estimator is a free and open-source project. You can use it without any limitations and without any hidden costs. You can also contribute to the project and help us make it even better.
                 </p>
             </div>
-            <div class="column"></div>
+            <div class="column">
+                <img src={feat5} alt="Azure Cost Estimator" />
+            </div>
         </div>
     </div>
 </section>
